b353a8ed51feca9a96e3ed10db992dcf8e30ed8f
Phil Gyford <phil@gyford.com>
2013-07-22 20:29:44 +0100

    Switch from jade to mustache for templating, get first tweets displaying

--- app.js
+++ app.js
@@ -3,11 +3,13 @@
  */
 var config = require('config'),
 		express = require('express'),
+		consolidate = require('consolidate'),
 		io = require('socket.io'),
 		http = require('http'),
 		twitter = require('ntwitter'),
 		cronJob = require('cron').CronJob,
 		_ = require('underscore'),
+		mustache = require('mustache')
 		path = require('path');
 
 //Create an express app
@@ -18,11 +20,38 @@ var server = http.createServer(app);
 
 var country = 'uk';
 
+// Will be an array of all the Twitter account numbers for all of the countries.
+// Like: [30313925, 138037459]
+var watched_accounts = _.uniq(
+		_.flatten(
+			_.map(config.countries, function(v, k, l) { return v.accounts; })
+		)
+);
+
+// Will map Twitter account numbers (as strings) to all the countries they're in.
+// Like: {'138037459': ['uk'], '30313925': ['us']}
+var account_to_country = {};
+//__.each(config.countries, function(country_data, country, l) {
+	//__.each(country_data.accounts, function(account) {
+		//account = account.toString();
+		//if (account in account_to_country) {
+			//account_to_country[account.toString()].push(country);
+		//} else {
+			//account_to_country[account] = [country];	
+		//}
+	//})
+//});
+
+var watchList = '';
+
 
 //Generic Express setup
 app.set('port', process.env.PORT || 3000);
-app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
+// Assign the mustache engine to .html files
+app.engine('html', consolidate.mustache);
+// Set .html as the default extension 
+app.set('view engine', 'html');
+app.set('views', path.join(__dirname, '/views'));
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
@@ -38,9 +67,9 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-//Our only route! Render it with the current watchList
+//Our only route! Render it with the current tweets.
 app.get('/', function(req, res) {
-	res.render('index', { data: watchList });
+	res.render('index', {static_data: {test: 'hello'}});
 });
 
 // Start a Socket.IO listen
@@ -56,7 +85,7 @@ if (config.env.heroku == true) {
 
 // If the client just connected, give them fresh data!
 sockets.sockets.on('connection', function(socket) { 
-    socket.emit('data', watchList);
+		socket.emit('tweets', [{text: 'a dummy first tweet'}]);
 });
 
 //Instantiate the twitter component
@@ -70,30 +99,18 @@ var t = new twitter({
     access_token_secret: config.twitter.access_token_secret 
 });
 
-// //Tell the twitter API to filter on the watchSymbols 
-t.stream('statuses/filter', { follow: config.countries[country].accounts}, function(stream) {
+// Tell the twitter API to filter on the watched_accounts. 
+t.stream('statuses/filter', { follow: watched_accounts}, function(stream) {
 
-  //We have a connection. Now watch the 'data' event for incomming tweets.
+  // We have a connection. Now watch the 'tweets' event for incoming tweets.
   stream.on('data', function(tweet) {
 
-    //This variable is used to indicate whether a symbol was actually mentioned.
-    //Since twitter doesnt why the tweet was forwarded we have to search through the text
-    //and determine which symbol it was ment for. Sometimes we can't tell, in which case we don't
-    //want to increment the total counter...
-    var claimed = false;
-
-    //Make sure it was a valid tweet
+    // Make sure it was a valid tweet
     if (tweet.text !== undefined) {
-			console.log(tweet.text);
+			console.log("FOUND: "+tweet.text);
 
-      //If something was mentioned, increment the total counter and send the update to all the clients
-      //if (claimed) {
-          ////Increment total
-          //watchList.total++;
-
-          ////Send to all the clients
-          //sockets.sockets.emit('data', watchList);
-      //}
+			//Send to all the clients
+			sockets.sockets.emit('tweets', [{text: tweet.text}]);
     }
   });
 });
@@ -108,7 +125,7 @@ t.stream('statuses/filter', { follow: config.countries[country].accounts}, funct
     //_.each(watchSymbols, function(v) { watchList.symbols[v] = 0; });
 
     ////Send the update to the clients
-    //sockets.sockets.emit('data', watchList);
+    //sockets.sockets.emit('tweets', watchList);
 //}, null, true);
 
 //Create the server
--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,17 +1,8 @@
 $(function() {
     var socket = io.connect(window.location.hostname);
-    socket.on('data', function(data) {
-        var total = data.total;
-        for (var key in data.symbols) {
-            var val = data.symbols[key] / total;
-            if (isNaN(val)) {
-                val = 0;
-            }
-            
-            $('li[data-symbol="' + key + '"]').each(function() {
-                $(this).css('background-color', 'rgb(' + Math.round(val * 255) +',0,0)');
-            });
-        }
-        $('#last-update').text(new Date().toTimeString());
+    socket.on('tweets', function(tweets) {
+			$.each(tweets, function(idx, tweet) {
+				$('#tweets').append('<li>' + tweet.text + '</li>');
+			});
     });
 })
